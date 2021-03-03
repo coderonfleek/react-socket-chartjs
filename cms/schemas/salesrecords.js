e54edd3719e94473579fae1433a3451b7b9b1c41
Fikayo <fikfikky@gmail.com>
2021-03-03 22:17:47 +0100

    Initial Commit

---
+++ cms/schemas/salesrecords.js
@@ -0,0 +1,17 @@
+export default {
+    name: "salesrecords",
+    title: "Sales Records",
+    type : "document",
+    fields : [
+        {
+            name: "month",
+            title: "Month",
+            type: "string"
+        },
+        {
+            name: "totalsales",
+            title: "Total Number of items sold",
+            type: "number"
+        }
+    ]
+}
